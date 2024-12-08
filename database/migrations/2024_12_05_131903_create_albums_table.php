b3f0113af68ce562ddb9c875478c7476cc204f45
ArturM2005 <arturmuhamediarov@gmail.com>
2024-12-08 05:36:27 +0300

    Gave album a foreigh key

--- database/migrations/2024_12_05_131903_create_albums_table.php
+++ database/migrations/2024_12_05_131903_create_albums_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('albums', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->unsignedBigInteger('song_id')->nullable();
+            $table->foreign('song_id', 'album_song_fk')->on('songs')->references('id');
         });
     }
 
